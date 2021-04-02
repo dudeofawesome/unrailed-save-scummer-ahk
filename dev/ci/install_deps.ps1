eea2e8238a2102264a69f393c17fc6151ab27fe9
Louis Orleans <louis@orleans.io>
2021-04-01 19:40:36 -0700

    🎨 format code

--- dev/ci/install_deps.ps1
+++ dev/ci/install_deps.ps1
@@ -4,7 +4,6 @@ if (Test-Path -Path "lib\") {
 }
 New-Item -Path "lib\" -ItemType directory > $null
 
-
 Invoke-WebRequest -Uri "https://autohotkey.com/download/ahk.zip" -OutFile "ahk.zip"
 Invoke-WebRequest -Uri "https://web.archive.org/web/20150511220826if_/http://www.matcode.com/mpress.219.zip" -OutFile "mpress.zip"
 Invoke-WebRequest -Uri "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe" -OutFile "lib\rcedit.exe"
