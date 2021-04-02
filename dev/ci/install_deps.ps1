3c80be44eb39cade0579ebc4e162e30bd6e29d3c
Louis Orleans <louis@orleans.io>
2021-04-01 19:39:56 -0700

    👷‍♂️🐛 fix action

--- dev/ci/install_deps.ps1
+++ dev/ci/install_deps.ps1
@@ -1,3 +1,10 @@
+# Delete old dependencies
+if (Test-Path -Path "lib\") {
+  Remove-Item -Recurse "lib\"
+}
+New-Item -Path "lib\" -ItemType directory > $null
+
+
 Invoke-WebRequest -Uri "https://autohotkey.com/download/ahk.zip" -OutFile "ahk.zip"
 Invoke-WebRequest -Uri "https://web.archive.org/web/20150511220826if_/http://www.matcode.com/mpress.219.zip" -OutFile "mpress.zip"
 Invoke-WebRequest -Uri "https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe" -OutFile "lib\rcedit.exe"
